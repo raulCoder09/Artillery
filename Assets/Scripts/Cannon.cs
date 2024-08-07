cad12672f38b7b9334db44472d5c7e0290971b1a
Adan Raul Morales Hernandez <adan.morales@udgvirtual.udg.mx>
2024-08-07 00:21:37 -0600

    working trace

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
+    public static bool blockShoot;
     [SerializeField] private GameObject bullet;
     private GameObject _shooter;
     private float _rotation;
-    private bool _onDownKey; //provisional
     private void Start()
     {
         _shooter = transform.Find("Shooter").gameObject;
@@ -20,17 +20,18 @@ public class Cannon : MonoBehaviour
         }
         if (_rotation > 90) _rotation = 90;
         if (_rotation < 0) _rotation = 0;
-        if (Input.GetKey(KeyCode.Space) && !_onDownKey)
+        if (Input.GetKey(KeyCode.Space) && !blockShoot)
         {
             
             var temp = Instantiate(bullet, _shooter.transform.position, transform.rotation);
             var tempRb = temp.GetComponent<Rigidbody>();
+            FollowCamera.target = temp;
             var shooterDirection = transform.rotation.eulerAngles;
             shooterDirection.y = 90 - shooterDirection.x;
             tempRb.linearVelocity = shooterDirection.normalized * GameManager.speedBall;
-            _onDownKey = true;
-        }else if(!Input.GetKey(KeyCode.Space))_onDownKey = false;
-
+            blockShoot = true;
+        }
+        
     }
     
 }
---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public static GameObject target;
+
+    [Header("Setting at editor")]
+    public float smoothing = 0.05f;
+    public Vector2 limitXY=Vector2.zero;
+
+    [Header("Dynamic setting")]
+    public float cameraZ;
+
+    private void Awake()
+    {
+        cameraZ = transform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 destiny;
+        if (target==null)
+        {
+            destiny=Vector3.zero;
+        }
+        else
+        {
+            destiny = target.transform.position;
+            if (target.CompareTag("Bullet"))
+            {
+                var sleeping = target.GetComponent<Rigidbody>().IsSleeping();
+                if (sleeping)
+                {
+                    target = null;
+                    destiny=Vector3.zero;
+                    Cannon.blockShoot = false;
+                    return;
+                }
+                destiny.x = Mathf.Max(limitXY.x, destiny.x);
+                destiny.y = Mathf.Max(limitXY.y, destiny.y);
+                destiny=Vector3.Lerp(transform.position, destiny, smoothing);
+                destiny.z = cameraZ;
+                transform.position = destiny;
+                if (Camera.main!=null) Camera.main.orthographicSize = destiny.y + 20;
+            }
+        }
+    }
+
+}
